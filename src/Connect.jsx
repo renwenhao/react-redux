fab41cfe2bd5c80d388bfb8603e8a055cd51d09e
renwenhao <renwenhao@sobey.com>
2018-06-28 18:25:27 +0800

    修改Connect.jsx

--- src/Connect.jsx
+++ src/Connect.jsx
@@ -15,17 +15,21 @@ const connect = (matlipltProps,operaProps) => (WrappleComponnet=React.Component)
         }
 
         componentWillMount(){
-            
+            const {store} = this.context;
+            const {dispatch} = store.dispatch;
+            let stateProps = matlipltProps(store.getState())
+            let operaPropss = operaProps(store.dispatch);
+            this.setState({
+                allProps:{
+                    ...stateProps,
+                    ...operaPropss,
+                    dispatch
+                }
+            });
         }
 
         render (){
-            const {store} = this.context;
-            let stateProps = matlipltProps(store.getState())
-            let operaPropss = operaProps(store.dispatch);
-            let waProps= {
-                ...stateProps,...operaPropss
-            }
-            return <WrappleComponnet {...waProps}/>
+            return <WrappleComponnet {...this.state.allProps}/>
         }
     }
     return Connect;
